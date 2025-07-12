4363dc5e2d44dc359c02d3e2a55e1c6a5e64c500
kevlp <kevlp@outlook.es>
2025-07-12 17:49:18 -0600

    Reemplazo de main por app

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="./font-awesome-4.7.0/css/font-awesome.min.css" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-  @vite('resources/css/main.css')
+  @vite('resources/css/app.css')
   <title>Movie Website | Animated Landing Page</title>
 </head>
 <body>
